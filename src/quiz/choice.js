8c22199231c8e11654fe07f1caa6c4e6e5875717
ajaythapliyal <ajaythapliyal1703@gmail.com>
2021-07-21 07:58:51 +0530

    reorganizing layouts, adding choice

---
+++ src/quiz/choice.js
@@ -0,0 +1,9 @@
+import m from 'mithril'
+
+const choice = {
+    view() {
+        return m('button', 'c#')
+    },
+}
+
+export default choice
--- src/quiz/quiz.js
+++ src/quiz/quiz.js
@@ -1,12 +1,26 @@
 import m from 'mithril'
+import choice from './choice'
 
 const quiz = {
     view() {
         return m('div', { class: 'quiz-container' }, [
             m('div', { class: 'quiz' }, [
-                m('div', [m('i', { class: 'fa fa-play fa-6x play-icon' })]),
+                m('i', { class: 'fa fa-play play-icon' }),
+                // m('div', { class: 'quiz-choice' }, [
+                //     m(choice),
+                //     m(choice),
+                //     m(choice),
+                //     m(choice),
+                // ]),
             ]),
         ])
+
+        // return m('div', { class: 'quiz-container' }, [
+        //     m('div', { class: 'quiz' }, [
+        //         m('i', { class: 'fa fa-play fa-6x play-icon' }),
+        //         [m(choice), m(choice)],
+        //     ]),
+        // ])
     },
 }
 
