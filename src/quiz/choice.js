2ff39ca3f9834bf970844d1d9bd0e2bc6668feac
ajaythapliyal <ajaythapliyal1703@gmail.com>
2021-08-02 07:47:09 +0530

    player and choices

--- src/quiz/choice.js
+++ src/quiz/choice.js
@@ -1,21 +1,11 @@
 import m from 'mithril'
-// import * as Tone from 'tone'
 
-const choice = {
-    view() {
-        return m(
-            'button',
-            {
-                class: 'choice',
-                // onclick: async () => {
-                //     await Tone.start()
-                //     const synth = new Tone.Synth().toDestination()
-                //     synth.triggerAttack('c#4', Tone.now())
-                // },
-            },
-            'c#'
-        )
-    },
+function choice(props) {
+    return {
+        view() {
+            return m('button', { class: 'choice' }, props.attrs.label)
+        },
+    }
 }
 
 export default choice
--- src/quiz/quiz.js
+++ src/quiz/quiz.js
@@ -1,18 +1,19 @@
 import m from 'mithril'
 import * as Tone from 'tone'
+import { sampleSize } from 'lodash'
 import choice from './choice'
 import { audioPlayerType, getAudioChoices } from './audioPlayer'
 
 function quiz() {
-    const audioChoices = getAudioChoices(audioPlayerType.single_note)
-    const note = `${audioChoices.sounds[0]}${audioChoices.octave}`
+    const { sounds, octave } = getAudioChoices(audioPlayerType.single_note)
+    const playedNote = `${sampleSize(sounds, 1)}${octave}`
     let isPlaying = false
     const synth = new Tone.Synth().toDestination()
     function togglePlay() {
         if (isPlaying) {
             synth.triggerRelease()
         } else {
-            synth.triggerAttack(note, Tone.now())
+            synth.triggerAttack(playedNote, Tone.now())
         }
         isPlaying = !isPlaying
     }
@@ -29,16 +30,13 @@ function quiz() {
                             class: isPlaying
                                 ? 'fa fa-pause-circle play-icon'
                                 : 'fa fa-play-circle play-icon',
-                            onclick: async () => {
-                                await Tone.start()
-                                togglePlay(note)
-                            },
+                            onclick: () => togglePlay(),
                         }),
                         m('div', { class: 'quiz-choice' }, [
-                            m(choice),
-                            m(choice),
-                            m(choice),
-                            m(choice),
+                            m(choice, { label: sounds[0] }),
+                            m(choice, { label: sounds[1] }),
+                            m(choice, { label: sounds[2] }),
+                            m(choice, { label: sounds[3] }),
                         ]),
                     ]
                 ),
