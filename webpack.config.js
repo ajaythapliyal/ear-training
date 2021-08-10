141566e5d99596e845d5213f99a8ab4e3ca9f2a8
ajaythapliyal <ajaythapliyal1703@gmail.com>
2021-08-10 08:12:38 +0530

    Configuring webpack to serve the app

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js',
+    },
     module: {
         rules: [
             {
