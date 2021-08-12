c4b3cc66454f9882883718248a35c0901fc1a89c
ajaythapliyal <ajaythapliyal1703@gmail.com>
2021-08-12 08:06:33 +0530

    Revert "Trying to make config work for prod"
    This reverts commit 0009e71049c7ae4d3e7a4c996fd8348a5d945569.

--- webpack.config.js
+++ webpack.config.js
@@ -9,28 +9,8 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'script-loader',
-                },
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: '[name]_[local]_[hash:base64]',
-                            sourceMap: true,
-                            minimize: true,
-                        },
-                    },
-                ],
+                enforce: 'pre',
+                use: ['source-map-loader'],
             },
         ],
     },
